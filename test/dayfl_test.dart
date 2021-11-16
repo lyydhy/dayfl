fe183e6cb362dcb09bee6c4d0de0ae8196a2da69
lyydhy <liu925494250@163.com>
2021-11-16 19:09:48 +0800

    移除无用的引用

--- test/dayfl_test.dart
+++ test/dayfl_test.dart
@@ -1,14 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2021-11-16 18:46:58
- * @LastEditTime: 2021-11-16 18:49:15
- * @LastEditors: your name
+ * @LastEditTime: 2021-11-16 19:09:29
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \dayfl\test\dayfl_test.dart
  */
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:dayfl/dayfl.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('dayfl');
