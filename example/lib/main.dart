3e577d23e6c512744cafe16d9883fc3099da7b2e
lyydhy <liu925494250@163.com>
2021-11-17 09:05:59 +0800

    1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-11-16 18:47:01
- * @LastEditTime: 2021-11-16 18:50:01
- * @LastEditors: your name
+ * @LastEditTime: 2021-11-17 09:04:52
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \dayfl\example\lib\main.dart
  */
@@ -21,7 +21,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     super.initState();
@@ -39,11 +38,12 @@ class _MyAppState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: [
+            Text("当前时间 :${Dayfl().format()}"),
             Text("当前时间 :${Dayfl().format('DD/MM/YYYY HH:mm:ss')}"),
             Text(
                 "指定时间 :${Dayfl('26-1-06', 'YY-M-DD').add(const Duration(days: 2)).format('YYYY-MM-DD HH:mm:ss')}"),
             Text(
-                "指定时间 :${Dayfl('26\\1/06 12:05:1', 'YY-M-DD HH:mm:s').format('YYYY-MM-DD HH:mm:ss')}")
+                "指定时间 :${Dayfl('26\\1/06 12:05:1', 'YY-M-DD HH:mm:s').format('YYYY-MM-DD HH:mm:ss')}"),
           ],
         ),
       ),
--- lib/dayfl.dart
+++ lib/dayfl.dart
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-16 11:48:22
- * @LastEditTime: 2021-11-16 18:49:10
+ * @LastEditTime: 2021-11-17 08:35:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \flutter_dayjs\lib\dayjs.dart
@@ -72,13 +72,13 @@ class Dayfl {
   /// 添加日期
   Dayfl add(Duration duration) {
     _datetime = _datetime.add(duration);
-    return Dayfl(_datetime);
+    return this;
   }
 
   /// 减去日期
   Dayfl subtract(Duration duration) {
     _datetime.subtract(duration);
-    return Dayfl(_datetime);
+    return this;
   }
 
   /// 时间差 两个时间相差 小时数
